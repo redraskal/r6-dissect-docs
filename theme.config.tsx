261e677c598b72c7717a612865a14e6548c6a0a6
redraskal <ben@ryben.dev>
2023-03-15 18:30:57 -0500

    more stuff

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>r6-dissect</span>,
@@ -12,6 +14,24 @@ const config: DocsThemeConfig = {
   docsRepositoryBase: 'https://github.com/redraskal/r6-dissect-docs/edit/main',
   footer: {
     text: 'Docs powered by Nextra',
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s - r6-dissect'
+    }
+  },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+        'https://dissect.redraskal.dev' +
+        (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+
+    return <>
+      <meta property="og:url" content={url} />
+      <meta property="og:title" content={frontMatter.title || 'r6-dissect'} />
+      <meta property="og:description" content={frontMatter.description || 'A Match Replay API & CLI for Rainbow Six: Siege\'s Dissect (.rec) format.'} />
+    </>
   },
 }
 
